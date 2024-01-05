b39efbf3dd9f1a749a0abfdd25987b544887c335
Chomtana <Chomtana001@gmail.com>
2024-01-06 01:22:51 +0700

    feat: default eas resolver

---
+++ src/resolver/attestor/OptiResolverEAS.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+import "@ethereum-attestation-service/eas-contracts/contracts/IEAS.sol";
+import "src/resolver/attestor/OptiResolverAttestorBase.sol";
+
+// TODO
+address constant EAS = 0x0000000000000000000000000000000000000000;
+
+contract OptiResolverEAS is OptiResolverAttestorBase {
+    function _read(bytes32 schema, address recipient, bytes memory header)
+        internal
+        view
+        virtual
+        override
+        returns (bytes memory)
+    {
+        bytes32 s = keccak256(abi.encode(schema, recipient, header));
+        bytes32 uid;
+
+        assembly {
+            uid := sload(s)
+        }
+
+        if (uid == bytes32(0)) return "";
+
+        Attestation memory data = IEAS(EAS).getAttestation(uid);
+
+        if (
+            data.uid == uid && data.schema == schema
+                && (data.expirationTime == 0 || block.timestamp <= data.expirationTime) && data.revocationTime == 0
+        ) {}
+    }
+
+    function _write(
+        bytes32 schema,
+        address recipient,
+        uint256 expiration,
+        bool revocable,
+        bytes memory header,
+        bytes memory body
+    ) internal virtual override returns (bytes32) {}
+
+    function _revoke(bytes32 schema, address recipient, bytes memory header)
+        internal
+        virtual
+        override
+        returns (bytes32)
+    {}
+}
