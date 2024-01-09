fcf630a8f0ce7ab2fbaea65edab49131d7f348e4
Chomtana <Chomtana001@gmail.com>
2024-01-10 00:17:03 +0700

    feat: use abi keccak salt instead of salt prefix

--- src/current/diamond/DiamondCloneFactory.sol
+++ src/current/diamond/DiamondCloneFactory.sol
@@ -13,26 +13,10 @@ contract DiamondCloneFactory is IDiamondCloneFactory {
     event CloneDiamond(address indexed cloner, address indexed resolver);
 
     /**
-     * @dev Modifier to ensure that the first 20 bytes of a submitted salt match
-     * those of the calling account. This provides protection against the salt
-     * being stolen by frontrunners or other attackers.
-     * @param salt bytes32 The salt value to check against the calling address.
-     */
-    modifier containsCaller(bytes32 salt) {
-        // prevent contract submissions from being stolen from tx.pool by requiring
-        // that the first 20 bytes of the submitted salt match msg.sender.
-        require(
-            (address(bytes20(salt)) == msg.sender),
-            "Invalid salt - first 20 bytes of the salt must match calling address."
-        );
-        _;
-    }
-
-    /**
      * Clone DiamondResolver to customize your own resolver
      */
-    function clone(bytes32 salt) public containsCaller(salt) {
-        address newResolver = Clones.cloneDeterministic(address(this), salt);
+    function clone(bytes32 salt) public {
+        address newResolver = Clones.cloneDeterministic(address(this), keccak256(abi.encodePacked(msg.sender, salt)));
         IDiamondResolverInitialize(newResolver).initialize(msg.sender, address(this));
         emit CloneDiamond(msg.sender, newResolver);
     }
