160f14e62bbbaeea3b63766728380c028abed906
Chomtana <Chomtana001@gmail.com>
2024-01-09 11:36:11 +0700

    feat: auto deploy patcher impl

--- src/patcher/PatcherProxy.sol
+++ src/patcher/PatcherProxy.sol
@@ -2,15 +2,16 @@
 pragma solidity ^0.8.8;
 
 import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import "src/patcher/Patcher.sol";
 
 interface PatcherInitialize {
     function initialize(address _owner) external;
 }
 
 contract PatcherProxy is TransparentUpgradeableProxy {
-    constructor(address _impl, address _owner)
+    constructor(address _owner)
         TransparentUpgradeableProxy(
-            _impl,
+            address(new Patcher()),
             address(this),
             abi.encodeWithSelector(PatcherInitialize.initialize.selector, _owner)
         )
