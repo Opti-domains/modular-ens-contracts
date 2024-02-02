3ace723658ec1af0ac2c9682daa63bde5f3a7728
Chomtana <Chomtana001@gmail.com>
2024-02-02 15:40:21 +0700

    feat: dev mode deploy script

--- script/DeployDev.s.sol
+++ script/DeployDev.s.sol
@@ -3,6 +3,9 @@ pragma solidity ^0.8.13;
 
 import {Script, console2} from "forge-std/Script.sol";
 import "src/current/diamond/DiamondResolver.sol";
+import "src/current/merkle/MerkleForest.sol";
+import "src/current/registry/ModularENSRegistry.sol";
+import "src/current/registrar/OpDomains.sol";
 
 contract DeployDevScript is Script {
     /// @notice Modifier that wraps a function in broadcasting.
@@ -17,5 +20,23 @@ contract DeployDevScript is Script {
     function run() public broadcast {
         // Deploy resolver factory
         Diamond diamond = new DiamondResolver(msg.sender);
+
+        MerkleForest merkleForest = new MerkleForest();
+        merkleForest.initialize();
+
+        ModularENSRegistry registry = new ModularENSRegistry(msg.sender, merkleForest, diamond);
+
+        OpDomains opDomains = new OpDomains(registry);
+        address opDomainsResolver = diamond.clone(bytes32(0));
+
+        registry.registerTLD(
+            ModularENS.TLD({
+                chainId: block.chainid,
+                nameHash: keccak256(abi.encodePacked(bytes32(0), keccak256(abi.encodePacked("op")))),
+                registrar: address(opDomains),
+                resolver: opDomainsResolver,
+                name: "op"
+            })
+        );
     }
 }
